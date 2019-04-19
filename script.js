342191baf11babadc7d5bc607c0b1f81b79a0c43
Egor <csgo.roby@gmail.com>
2019-04-19 13:43:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let questionOne = prompt("Введите обязательную статью расходов в этом месяце");
+let questionTwo = prompt("Во сколько обойдется");
+
+let appData = {
+	budjet: money,
+	typeData: time,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	saving: false
+};
+
+let moneyData = questionOne,
+typeDate = questionTwo;
+
+appData.expenses[moneyData] = typeDate;
+
+console.log(appData.expenses);
